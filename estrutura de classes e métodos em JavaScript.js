f106d97b26abf575a21ec120ea6d42be484290be
felipeoshiro2004 <79027420+felipeoshiro2004@users.noreply.github.com>
2021-09-14 08:01:17 -0300

    Add files via upload

---
+++ estrutura de classes e métodos em JavaScript.js
@@ -0,0 +1,156 @@
+class Nave{
+
+vidas;
+posX,posY;
+cor;
+formato;			
+colisao;
+
+MoverNave(){
+		return mover;
+		return posX;
+		return posY;
+//FUNÇÃO:determina o movimento da nave
+}
+ConquistarVidas (){
+		
+//FUNÇÃO:detrmina se o jogador ira receber vidas
+}
+DesenhaNave(){
+		
+//FUNÇÃO:definir o desenho da nave
+}
+AtacarInimigos(){
+
+//FUNÇÃO:determina o ataque que o jogador ira dar ao inimigo
+}
+ControlaNave(){
+		
+//FUNÇÃO:determina os controles da nave
+}
+DestruirNave(){
+
+//FUNÇÃO:determina quando a nave for destruida
+}
+VerificaColisaoNave(){
+		return posicaoX;
+		returnposicaoY;
+//FUNÇÃO:determina quando a nave for colidida com algum objeto
+} 
+}
+
+class Inimigos{
+
+cor;
+velocidade; 
+formato;
+colisao;
+posX,posY
+
+MoverInimigos(){
+		return mover;
+		return posX;
+		return posY;
+//FUNÇÃO:designa o Movimento inimigos
+}
+MudarVelocidade(){
+		return velocidade;
+//FUNÇÃO:alterar a velocidade
+}
+AlterarFormato(){
+		return formato
+//Função:definir o formato 
+}
+DesenharInimigos(){
+
+//FUNÇÃO:definir o desenho dos inimigos
+}
+AtacarNave(){
+
+//FUNÇÃO:determina o ataque que o inimigo ira dar ao jogador
+}
+ReiniciaInimigos(){
+
+//FUNÇÃO:reiniciar os inimigos
+}
+VerificaColisaoInimigos(){
+		return posicaoX;
+		return posicaoY;
+//FUNÇÃO:verificar onde o inimigo ira colidir com tal objeto
+}
+}
+
+
+class Espaço{
+
+linhas, pontos, statusJogo,coluna;
+teclas;
+
+IniciaJogo(){
+
+//FUNÇÃO:começar o jogo 
+}
+TerminaJogo(){
+
+//FUNÇÃO:finaliza o jogo
+}
+ConquistaPontos(){
+	return pontos;
+//FUNÇÃO:determina a quantidade de pontos que o jogador ira ganhar
+}
+constructor DesenhaEspaço(){
+		return linhas;
+		return colunas;
+//FUNÇÃO:desenhar o cenario com valores de linhas e colunas
+}
+CalculaPontos(){
+		return pontos;
+//FUNÇÃO:calcula a quantidade de pontos que o jogador ira receber
+}
+ExibePontos(){
+		return pontos;
+//FUNÇÃO:Exibe a pontuação
+}
+}
+
+class Obstáculos{
+obstaculos;
+formato;
+posX,posY;
+
+ObstaculosQuebrados(){
+		return posicaoX;
+		return posicaoY;
+//FUNÇÃO:determina quais objetos foram quebrados
+}
+DesenhaObstaculos(){
+		
+//FUNÇÃO:desenhar os obstaculos
+}
+ObstaculosDestruidos(){
+		return posicaoX;
+		return posicaoY;
+//FUNÇÃO:determina quais objetos foram destruidos
+}
+}
+ 
+classe Mísseis{
+colisao;
+direcao;
+formato;
+posX,posY;
+
+DefinirFormato(){
+		return formato
+//FUNÇÃO:definir o formato 
+}
+DefinirDirecao(){
+		return direcao
+//FUNÇÃO:definir a direcao
+}
+VerificaColisaoMisseis(){
+		return posicaoX;
+		returnposicaoY;
+//FUNÇÃO:determina quando os misseis for colidida com algum objeto
+}
+}
